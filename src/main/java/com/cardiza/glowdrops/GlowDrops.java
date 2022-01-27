8bc0b949e25f862ad65400f425cc600559f784fc
acarneirod <96859840+cardiza@users.noreply.github.com>
2022-01-27 15:17:14 +0100

    Starting the project

---
+++ src/main/java/com/cardiza/glowdrops/GlowDrops.java
@@ -0,0 +1,17 @@
+package com.cardiza.glowdrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GlowDrops extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
